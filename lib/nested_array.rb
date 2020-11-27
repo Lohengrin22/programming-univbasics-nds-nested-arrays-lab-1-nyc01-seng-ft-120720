3ad0712849e2569742955cd9e335c2f3bafb4fa0
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-27 18:23:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -84,8 +84,8 @@ def matrix_lookup(matrix, row, column)
   "Asparagus"
 ]
  ] 
- row = matrix[i]
- column = row[i]
+ row = matrix[]
+ column = row[]
 end
 
 def matrix_update(matrix, row, column, new_value)
