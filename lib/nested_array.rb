0c66a345a1de583afa08ab364a134a52ab1dbec1
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-27 18:33:15 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -69,9 +69,7 @@ def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
   array = []
-
-sorted = [array[0].sort,array[1].sort]
-sorted[row][column]
+array[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
